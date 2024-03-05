d3b6133a20749f7d98a718defbd3cb95b2fa8247
Divya <glishasunil@gmail.com>
2024-03-05 18:35:10 +0000

    second commit

--- Hello/src/com/practise/HelloEorld.java
+++ Hello/src/com/practise/HelloEorld.java
@@ -4,6 +4,8 @@ public class HelloEorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello friend");
+		System.out.println("Welcome to First Java program!!!");
 
 	}
 
